8bb18b36ee486d995e3af1f66371d6aa4e2c27af
Harun HATİBOĞLU <111196660+Harhat18@users.noreply.github.com>
2022-10-07 00:43:48 +0300

    add

--- src/App.js
+++ src/App.js
@@ -2,11 +2,24 @@ import { useState } from "react";
 
 function App() {
   const [name, setName] = useState("Harun");
-
+  const [age, setAge] = useState("30");
+  const [friends, setFriends] = useState(["Ali", "Mehmet"]);
   return (
     <div className="App">
       <h1>Merhaba {name}!</h1>
-      <button onClick={() => setName("Fatma")}>Click</button>
+      <h2>{age}</h2>
+      <button onClick={() => setName("Fatma")}>change name</button>
+      <button onClick={() => setAge("29")}>change age</button>
+
+      <hr />
+      <br></br>
+      <h2>FRİENDS</h2>
+      {friends.map((friend, index) => (
+        <div key={index}>{friend}</div>
+      ))}
+      <button onClick={() => setFriends([...friends, "Hüseyin"])}>
+        Add New Friend
+      </button>
     </div>
   );
 }
